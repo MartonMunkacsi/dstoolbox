2cfcb6265db3498c71b923d51704ac39d166dc0d
Marton <marton.munkacsi09@gmail.com>
2023-06-10 02:22:33 +0100

    modified setup

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+import os
+import sys
 
 VERSION = '0.0.1' 
 DESCRIPTION = 'A toolbox for Data Science.'
@@ -6,22 +8,23 @@ LONG_DESCRIPTION = 'A Data Science Toolbox with helpful functionality in preproc
 
 # Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
-        name="dstoolbox", 
-        version=VERSION,
-        author="Marton Munkacsi",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=['pandas', 'numpy', 'spacy', 'scikit-learn'], # add any additional packages that 
-        # needs to be installed along with your package. Eg: 'caer'
-        
-        keywords=['python', 'data science', 'toolbox', 'preprocessing', 'data exploration'],
-        classifiers= [
-            "Development Status :: 1 - Planning",
-            "Intended Audience :: Data Enthusiasts",
-            "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-        ]
+  name="dstoolbox", 
+  version=VERSION,
+  author="Marton Munkacsi",
+  description=DESCRIPTION,
+  long_description=LONG_DESCRIPTION,
+  packages=find_packages(),
+  install_requires=['pandas', 'numpy', 'spacy', 'scikit-learn'],
+  keywords=['python', 'data science', 'toolbox', 'preprocessing', 'data exploration'],
+  classifiers= [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Data Enthusiasts",
+    "Programming Language :: Python :: 3",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+  ]
 )
+
+# Download en_core_web_sm model
+if 'install' in sys.argv:
+    os.system('python -m spacy download en_core_web_sm')
